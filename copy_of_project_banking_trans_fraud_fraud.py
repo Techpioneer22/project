a69cb5604508d114ebb88c20df72333bd7d4816c
Techpioneer22 <emmanuelisaacbanda@gmail.com>
2021-07-07 14:03:29 +0200

    Update copy_of_project_banking_trans_fraud_fraud.py

--- copy_of_project_banking_trans_fraud_fraud.py
+++ copy_of_project_banking_trans_fraud_fraud.py
@@ -17,9 +17,7 @@ from keras.layers import Dense
 from keras.layers import Dropout
 import seaborn as sn
 import matplotlib as pt
-!conda create -n tensorflow python=3.5
-!activate tensorflow
-!pip install --ignore-installed --upgrade tensorflow
+!pip install tensorflow==1.2.0 --ignore-installed
 
 """Compiled by:
 1.  Perceviarance Chitima R195807D
